687af0474a534a55e9e671443d5354816c0fcfcc
gwendolynkwong <gwen.kwong1@gmail.com>
2022-10-31 17:33:38 -0400

    Added SoundInteractor file

---
+++ src/main/SoundInteractor.java
@@ -0,0 +1,14 @@
+public class SoundInteractor {
+
+    /**
+     * Plays audio file.
+     */
+    public static void playSound(String filepath) {
+        try {
+
+        }
+        catch(Exception e) {
+            // show error message
+        }
+    }
+}
