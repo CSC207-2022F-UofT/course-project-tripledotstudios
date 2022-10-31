afb14d9373229177e128de81a361e63d55cf5af5
gwendolynkwong <gwen.kwong1@gmail.com>
2022-10-31 17:49:49 -0400

    Added LoginInteractor file

--- src/main/java/LoginInteractor.java
+++ src/main/java/LoginInteractor.java
@@ -1,9 +1,11 @@
 public class LoginInteractor {
 
     /**
-     *
+     * Returns true iff username and password match each other.
+     * @param username
+     * @param password
      */
-    public void validateLogin() { // should be boolean
+    public void validateLogin(String username, String password) { // should be boolean
 
     }
 
