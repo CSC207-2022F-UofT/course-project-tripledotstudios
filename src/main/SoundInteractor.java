58e4779c8e2c9ca57ed1e2a832d464e6ba450f36
gwendolynkwong <gwen.kwong1@gmail.com>
2022-11-08 16:27:42 -0500

    Added constructor and stopSound, edited playSound

--- src/main/SoundInteractor.java
+++ src/main/SoundInteractor.java
@@ -3,21 +3,40 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class SoundInteractor {
+    private static Media sound;
+    private static MediaPlayer mediaPlayer;
+    /**
+     *
+     */
+    public SoundInteractor(String filepath) {
+        try {
+            // these all need to be static variables!
+            this.sound = new Media(filepath);
+            this.mediaPlayer = new MediaPlayer(sound);
+            mediaPlayer.play();
+        }
+        catch(Exception e) {
+            System.out.println(""); // add error message
+        }
+    }
 
     /**
      * Plays audio file.
      */
-    public static void playSound(String filepath) {
-        // need to stop any sound that is already playing first
+    public static void playSound() {
         try {
-            Media sound = new Media(filepath);
-            MediaPlayer mediaPlayer = new MediaPlayer(sound);
-            mediaPlayer.play();
+
         }
         catch(Exception e) {
             System.out.println("Could not play sound.");
         }
     }
 
+    /**
+     * Stops playing audio file.
+     */
+    public static void stopSound() {
+
+    }
 
 }
