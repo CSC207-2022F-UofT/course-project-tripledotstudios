75b1d42ded9b67dbb0c955d4a0e7de5d405ebd48
gwendolynkwong <gwen.kwong1@gmail.com>
2022-11-14 18:44:00 -0500

    Merged main to branch

---
+++ src/main/java/ItemDataManager.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ItemDataManager class that reads through the ItemData csv and stores objects of type ItemData
+ * in a list.
+ */
+public class ItemDataManager{
+    public List<ItemData> lst = new ArrayList<>(); // List of ItemData Objects;
+
+    public ItemDataManager(String fileName){
+        String line = "";
+        String splitBy = ",";
+
+        try{
+            //parsing a CSV file into BufferedReader class constructor
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            while ((line = br.readLine()) != null)   //returns a Boolean value
+            {
+                String[] e = line.split(splitBy);    // use comma as separator
+                ItemData item = new ItemData(e[0], e[1], Integer.parseInt(e[2]));
+                lst.add(item);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * method that returns the list of ItemData objects made from ItemManager
+     * @return list
+     */
+    public List<ItemData> getItemData(){
+        return this.lst;
+    }
+}
