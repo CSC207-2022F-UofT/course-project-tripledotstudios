dff8658e5d69023cfa8c69e8406d0518adbcd7b3
SleepyyPlayz <16306416+SleepyyPlayz@users.noreply.github.com>
2022-12-03 21:56:06 -0500

    all warnings dealt with in entities package, also removed tutorial folder

--- src/main/java/entities/PlayerData.java
+++ src/main/java/entities/PlayerData.java
@@ -12,16 +12,18 @@ public class PlayerData implements Serializable {
     /* Stores the health of the entities.PlayerData object */
     // public static final int MAX_HEALTH = 500;
 
-    /** Stores the attack power of the entities.PlayerData object */
+    // Stores the attack power of the entities.PlayerData object
     private int attackPower;
 
-    /** The current event */
+    // The current event
     private int eventID;
 
-    /** The username of the player */
+    // The username of the player
     private final String username;
 
-    /** The inventory of the entities.PlayerData stores the name of the item mapped to the entities.ItemData object */
+    // The inventory of the entities.PlayerData stores the
+    // name of the item mapped to the entities.ItemData object
+    @SuppressWarnings("all")
     private HashMap<String, ArrayList<ItemData>> inventory;
 
     /**
@@ -81,6 +83,7 @@ public class PlayerData implements Serializable {
      * Creates a new list of entities.ItemData if there is not already one.
      * @param item The entities.ItemData object that is added to the entities.PlayerData's inventory.
      */
+    @SuppressWarnings("all")
     public void addToInventory(ItemData item) {
         // if the item is not already in the map, create a list for it.
         if (!inventory.containsKey(item.getName())) {
@@ -98,7 +101,7 @@ public class PlayerData implements Serializable {
      */
     public boolean removeFromInventory(ItemData item) {
         // boolean to determine if the value was returned successfully.
-        boolean removed = false;
+        boolean removed;
         // assign a pointer to the list contained in the hashmap
         ArrayList<ItemData> pointer = inventory.get(item.getName());
 
